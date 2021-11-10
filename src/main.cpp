b6be14575e8d463190c1b954fed5072c83d654cb
Thanh Pham <thanhphamlike@gmail.com>
2021-11-10 17:39:29 -0500

    moved main.cpp to src and modified gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 int main() {
+	string filePath;
+	ifstream inF;
+	inF.open(filePath);
+	if (inF.fail()) exit(1);
+	do {
+		char c;
+		inF.get(c);
+#if 0
+		if (c belongs to instruction) {
+			// do this
+		}
+		else {
+			// do this
+		}
+#endif // 0
 
-	cout << "hello world";
+		cout << c;
+	} while (!inF.eof());
 	return 0;
 }
