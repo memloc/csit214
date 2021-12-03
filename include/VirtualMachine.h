104a9d9807bb4e805111313ba99a1705581819ed
memloc <81724678+memloc@users.noreply.github.com>
2021-12-02 21:34:47 -0500

    begin implementing the virtual machine

--- include/VirtualMachine.h
+++ include/VirtualMachine.h
@@ -1,59 +1,82 @@
 #ifndef VIRTUAL_MACHINE_H
 #define VIRTUAL_MACHINE_H
 
+#include <cstdint>
+#include <iostream>
 #include <stack>
 #include <vector>
-#include <iostream>
 
 #include "Bytecode.h"
+#include "Compiler.h"
 
 namespace VM {
 
-  using namespace std;
+enum Exception {
+  STACK_OVERFLOW,
+  STACK_UNDERFLOW,
+};
 
-  enum class WordType:uint8_t {
-    CHAR_T, 
-    INT_T,
-  };
+using namespace std;
 
-  /**
-   * @brief A fixed size unit of data (64-bits)
-   * @param type the intended interpretation of the data, one of WordType.
-   */
-  typedef struct Word {
-      WordType type;
+enum class WordType : uint8_t {
+  INT32_T,
+  CHAR32_T,
+  CHAR_T,
+};
 
-      union {
-        int8_t asChar;
-        int32_t asInt;
-      };
-  } Word;
+/* Extra 16-bits left-over to put inside the WORD
+enum class WordFlags:uint8_t {};
+*/
 
-  class VirtualMachine {
-  private:
-    // keep track of program execution progress (program counter)
-    int32_t counter; 
+/*
+ * @brief A fixed size unit of memory to store data (64-bits)
+ * The intended interpretation of the data being the WordType.
+ */
+typedef struct Word {
+  WordType type;
+  union {
+    int32_t asInt32;
+    uint32_t asUInt32;
+    struct asChar32 {
+      int8_t $ch1; // 'o'
+      int8_t $ch2; // 'o'
+      int8_t $ch3; // 'p
+      int8_t $ch4; // 's' Must null terminate '\0'
+    } asChar32;
+  } memory;
+} Word;
 
-    // stack pointer
-    Word *sp;
-    stack<Word> data;
+class VirtualMachine {
+private:
+  // keep track of program execution progress (program counter)
+  int32_t counter;
 
-    // instruction pointer
-    Bytecode::Instruction *ip; 
-    vector<Bytecode::Instruction>* code;
+  // Stack pointer
+  Word *sp;
+  stack<Word> data;
 
-    // halt execution flag
-    bool halt; 
+  // Instruction pointer
+  Bytecode::Instruction *ip;
+  vector<Bytecode::Instruction> *bytecode;
 
-  private:
-    // execute a single instruction
-    void step(Bytecode::Instruction);
+  // Halt execution flag
+  bool halt;
 
-  public:
-    void load(/* bytecode source code */);
-    void run();
-  };
+public:
+  VirtualMachine(vector<Bytecode::Instruction> *);
+  void run();
 
-}
+private:
+  // load a single instruction
+  void load(Bytecode::Instruction);
+  // execute a single instruction
+  void step();
+
+private:
+  // convert the data in an instruction into a word
+  vector<Word> convert(Bytecode::Instruction);
+};
+
+} // namespace VM
 
 #endif // VIRTUAL_MACHINE_H
